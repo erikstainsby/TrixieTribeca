bad2bcba92730588db2075020370c851d2790569
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-04-28 17:12:55 -0700

    Refreshed plugins - added loader
    Signed-off-by: Erik Stainsby <erik.stainsby@roaringsky.ca>

--- Trixie/RSPopoverController.m
+++ Trixie/RSPopoverController.m
@@ -31,6 +31,14 @@
 @synthesize filterPanel;
 @synthesize reactionPanel;
 
+@synthesize actionMenu;
+@synthesize filterMenu;
+@synthesize reactionMenu;
+
+@synthesize actionPlugins;
+@synthesize filterPlugins;
+@synthesize reactionPlugins;
+
 
 #ifndef RSPopover_constants
 #define RSPopover_constants
@@ -49,6 +57,9 @@ const CGFloat	viewMargin = 10;
 - (void) awakeFromNib {
 	
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showPopover:) name:RSPopoverRequestedNotification object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeActiveActionPlugin:) name:RSTrixieChangeActiveActionPluginNotification object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeActiveFilterPlugin:) name:RSTrixieChangeActiveFilterPluginNotification object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeActiveReactionPlugin:) name:RSTrixieChangeActiveReactionPluginNotification object:nil];
 	
 	_popoverController = [[SFBPopoverWindowController alloc] initWithWindow:popoverWindow];
 	
@@ -61,9 +72,39 @@ const CGFloat	viewMargin = 10;
 	[[_popoverController popoverWindow] setDrawsArrow:drawsArrow];
 	[[_popoverController popoverWindow] setViewMargin:floorf(viewMargin)];
 	
+	RSTrixieLoader * loader = [[RSTrixieLoader alloc] init];
+	actionPlugins = [loader loadPluginsWithPrefix:@"Action" ofType:@"bundle"];
+	filterPlugins = [loader loadPluginsWithPrefix:@"Filter" ofType:@"bundle"];
+	reactionPlugins = [loader loadPluginsWithPrefix:@"Reaction" ofType:@"bundle"]; 
+	loader = nil;
+	
+	NSMenu * m1 = [[NSMenu alloc] initWithTitle:@"Actions"];
+	for( RSActionPlugin * p in actionPlugins) {
+		NSMenuItem * item = [[NSMenuItem alloc] initWithTitle:[p pluginName] action:@selector(setActiveActionPlugin:) keyEquivalent:@""];
+		[item setRepresentedObject: p];
+		[m1 addItem:item];
+	}
+	[actionMenu setMenu:m1];
+	
+	NSMenu * m2 = [[NSMenu alloc] initWithTitle:@"Actions"];
+	for( RSFilterPlugin * p in filterPlugins) {
+		NSMenuItem * item = [[NSMenuItem alloc] initWithTitle:[p pluginName] action:@selector(setActiveFilterPlugin:) keyEquivalent:@""];
+		[item setRepresentedObject: p];
+		[m2 addItem:item];
+	}
+	[filterMenu setMenu:m2];
+	
+	NSMenu * m3 = [[NSMenu alloc] initWithTitle:@"Actions"];
+	for( RSReactionPlugin * p in reactionPlugins) {
+		NSMenuItem * item = [[NSMenuItem alloc] initWithTitle:[p pluginName] action:@selector(setActiveReactionPlugin:) keyEquivalent:@""];
+		[item setRepresentedObject: p];
+		[m3 addItem:item];
+	}
+	[reactionMenu setMenu:m3];
+
 }
 
-- (IBAction) swapPlugin:(id)sender {
+- (IBAction) swapPluginPanel:(id)sender {
 	
 	/**
 	 *	 [ A | F | R ] segment control
@@ -121,6 +162,37 @@ const CGFloat	viewMargin = 10;
 		[_popoverController displayPopoverInWindow:_window atPoint:where chooseBestLocation:YES];
 		
 	}
+}
+
+- (void) changeActiveActionPlugin:(NSNotification*)nota {
+	RSActionPlugin * p = [nota object];
+	if(nil != actionPlugin) {
+		[actionPanel replaceSubview:[actionPlugin view] with:[p view]];
+	}
+	else {
+		[actionPanel addSubview:[p view]];
+	}
+	actionPlugin = p;
+}
+- (void) changeActiveFilterPlugin:(NSNotification*)nota {
+	RSFilterPlugin * p = [nota object];
+	if(nil != filterPlugin) {
+		[filterPanel replaceSubview:[filterPlugin view] with:[p view]];
+	}
+	else {
+		[filterPanel addSubview:[p view]];
+	}
+	filterPlugin = p;
+}
+- (void) changeActiveReactionPlugin:(NSNotification*)nota {
+	RSReactionPlugin * p = [nota object];
+	if(nil != reactionPlugin) {
+		[reactionPanel replaceSubview:[reactionPlugin view] with:[p view]];
+	}
+	else {
+		[reactionPanel addSubview:[p view]];
+	}
+	reactionPlugin = p;
 }
 
 - (void) windowDidResize:(NSNotification *)notification {
